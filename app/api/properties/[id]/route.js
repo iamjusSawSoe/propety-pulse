5744255e1f2efd5e660bb002bac034abe9b465c5
SAW SOE HTUT WIN <sawsoehtutwin78@gmail.com>
2024-07-14 14:21:54 +0630

    complete properties detail page

---
+++ app/api/properties/[id]/route.js
@@ -0,0 +1,19 @@
+import connectDB from "../../../../config/database";
+import Property from "../../../../models/Property";
+
+// GET /api/properties/:id
+export const GET = async (request, { params }) => {
+  await connectDB();
+
+  const property = await Property.findById(params.id);
+
+  if (!property) return new Response("Property not found", { status: 404 });
+
+  try {
+    return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,9 +1,14 @@
 import connectDB from "../../../config/database";
+import Property from "../../../models/Property";
 
+// GET /api/properties
 export const GET = async (request) => {
   await connectDB();
+
+  const properties = await Property.find({});
+
   try {
-    return new Response(JSON.stringify({ message: "Hello World" }), {
+    return new Response(JSON.stringify(properties), {
       status: 200,
     });
   } catch (error) {
